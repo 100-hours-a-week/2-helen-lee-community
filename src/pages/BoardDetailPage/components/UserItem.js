9948fbb4821526d1fc67a0da1498113d4a673790
hyemimi <hyemi7375@gmail.com>
2025-03-25 11:31:55 +0900

    feat: 게시글 삭제

--- src/pages/BoardDetailPage/components/UserItem.js
+++ src/pages/BoardDetailPage/components/UserItem.js
@@ -1,6 +1,13 @@
 import Button from "../../../components/Button";
 import CONFIG from "../../../config";
+import { navigateTo } from "../../../router";
 
+/** 재사용되는 유저 프로필 항목.
+ * 
+ * 현재 로그인 된 유저의 항목일 경우:
+ * 댓글일 경우 post_id, comment_id가 parameter 로 전달되고 
+ * 게시글에서 사용될 경우 comment_id가 전달되지 않아, 이를 기반으로 구분한다.
+ **/
 export default function UserItem ({nickname, profile_image, created_at,comment_id, post_id}) {
 
 
@@ -26,31 +33,33 @@ export default function UserItem ({nickname, profile_image, created_at,comment_i
 
     // 계정 주인의 유저 정보일 경우
     if (UserItemButtonDiv) {
-          /** 댓글 삭제 */
-        const UserItemDeleteButton = Button({
-            text: "삭제",
-            onClick: () => {deleteComment(comment_id,post_id)},
-            className: "userItem-edit-button",
-        });
-
         const UserItemEditButtonDiv = userItemMeta.querySelector(".userItem-edit-button-div");
-        UserItemEditButtonDiv.appendChild(UserItemDeleteButton);
+
+        // 댓글 항목일 경우
+        if (comment_id) {
+             /** 댓글 삭제 */
+            const UserItemCommentDeleteButton = Button({
+                text: "삭제",
+                onClick: () => {deleteComment(comment_id,post_id)},
+                className: "userItem-edit-button",
+            });
+
+            UserItemEditButtonDiv.appendChild(UserItemCommentDeleteButton);
+        }
+
+        // 게시글 항목일 경우
+        else if (post_id) {
+            const UserItemPostDeleteButton = Button({
+                text: "삭제",
+                onClick: () => {deletePost(post_id)},
+                className: "userItem-edit-button",
+            });
+
+            UserItemEditButtonDiv.appendChild(UserItemPostDeleteButton);
+        }  
     }
-    
-   
 
-    /** 댓글 수정 */
-    // const UserItemEditButton = Button({
-    //     text: "삭제",
-    //     onClick: deleteComment(),
-    //     className: "userItem-edit-button"
-    // });
-
-    // const userItemEditButtonDiv = document.getElementById("userItem-edit-button-div");
-    // userItemEditButtonDiv.appendChild(userItemDeleteButton);
-    
-
-     /** 댓글 삭제 함수 */
+     /** 댓글 삭제 API FETCH */
      async function deleteComment (comment_id, post_id) {
         try {
             const res = await fetch(`${CONFIG.API_URL}/posts/${post_id}/comment/${comment_id}`,
@@ -61,6 +70,24 @@ export default function UserItem ({nickname, profile_image, created_at,comment_i
 
             alert("댓글이 삭제되었습니다");
             window.location.reload();
+
+        } catch (err) {
+            console.log(err);
+            alert("댓글 삭제 실패");
+        }
+    }
+
+     /** 게시글 삭제 API FETCH */
+     async function deletePost (post_id) {
+        try {
+            const res = await fetch(`${CONFIG.API_URL}/posts/${post_id}`,
+                {method : "DELETE"}
+            );
+            
+            if (!res.ok) throw new Error("게시글 삭제 실패");
+
+            alert("게시글이 삭제되었습니다");
+            navigateTo("/boards")
 
         } catch (err) {
             console.log(err);
