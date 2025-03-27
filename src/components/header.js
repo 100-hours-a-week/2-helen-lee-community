0ba5a674cfec30f6256a8553a9bdae909acf6d85
hyemimi <hyemi7375@gmail.com>
2025-03-27 13:48:17 +0900

    feat: 헤더 뒤로가기 메뉴 추가, 스타일 수정

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,12 @@
 import { navigateTo } from "../router.js";
+import backButton from '../assets/back_button.png';
 
 export default function Header () {
     const header = document.getElementById("header");
     
     header.innerHTML = `
     <div class="header-div">
+        <img id="back-button" src=${backButton} alt="" height="45"/>
         <h1>조잘조잘</h1>
         <div id="profile"></div>
     </div>
@@ -13,8 +15,14 @@ export default function Header () {
 
     const Profile = document.getElementById("profile")
     const ProfileImage = document.createElement("img")
-    ProfileImage.src = JSON.parse(sessionStorage.getItem("user")).profile_image_url;
-    ProfileImage.classList.add("profile-image")
+
+    /** 유저 프로필 */
+    if (sessionStorage.getItem("user")) {
+        ProfileImage.src = JSON.parse(sessionStorage.getItem("user")).profile_image_url;
+        ProfileImage.classList.add("profile-image")
+    }
+   
+    
     Profile.appendChild(ProfileImage)
 
     ProfileImage.addEventListener("click", ()=> navigateTo('/profile'))
@@ -27,5 +35,9 @@ export default function Header () {
             header.classList.remove("header-scrolled");
         }
     });
+
+    /** 뒤로 가기 이벤트 */
+    const BackButton = document.getElementById("back-button");
+    BackButton.addEventListener('click', () => history.back())
 
 }
